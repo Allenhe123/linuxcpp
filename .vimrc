449812431df89fade836f626d5adb61f0b5116f3
Allenhe123 <931317255@qq.com>
2017-12-05 00:20:46 -0500

    up

---
+++ .vimrc
@@ -0,0 +1,7 @@
+syntax on
+set nu
+set tabstop=4
+set shiftwidth=4
+set smartindent
+set cindent
+set hlsearch
--- LinuxServerCodes/9/9-6mytalk_client.cpp
+++ LinuxServerCodes/9/9-6mytalk_client.cpp
@@ -39,7 +39,7 @@ int main( int argc, char* argv[] )
     }
 
     pollfd fds[2];
-    fds[0].fd = 0;
+    fds[0].fd = 0; // STDIN
     fds[0].events = POLLIN;
     fds[0].revents = 0;
     fds[1].fd = sockfd;
@@ -67,13 +67,16 @@ int main( int argc, char* argv[] )
         else if( fds[1].revents & POLLIN )
         {
             memset( read_buf, '\0', BUFFER_SIZE );
+			// sockfd --> readbuf
             recv( fds[1].fd, read_buf, BUFFER_SIZE-1, 0 );
             printf( "%s\n", read_buf );
         }
 
         if( fds[0].revents & POLLIN )
         {
+			// STDIN --> pipefd[1]
             ret = splice( 0, NULL, pipefd[1], NULL, 32768, SPLICE_F_MORE | SPLICE_F_MOVE );
+			// pipefd[0] --> sockfd
             ret = splice( pipefd[0], NULL, sockfd, NULL, 32768, SPLICE_F_MORE | SPLICE_F_MOVE );
         }
     }
@@ -81,3 +84,4 @@ int main( int argc, char* argv[] )
     close( sockfd );
     return 0;
 }
+
